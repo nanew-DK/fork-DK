8776606535a1f09bcf0a597e8d8b0afcb1be2085
jj2311029 <151961300+jj2311029@users.noreply.github.com>
2024-11-06 16:55:38 +0900

    호진님 hp구현+ 로프시도

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,30 +2,49 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental;
+using UnityEditor.Experimental.GraphView;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
-    private bool isFacingRight = true;
+    //플레이어 좌우 이동
+    [SerializeField] private float speed = 8f;//플레이어 스피드
+    private float moveInput = 0f;//플레이어 좌우이동 input
+    private bool isFacingRight = true;//좌우 처다보는것
+
+    //플레이어 점프
+    private float jumpingPower = 16f;//점프 높이
+    
+    //플레이어 대쉬
     private bool isDash=false;
     private bool canDash = true;
-    public float dashDuration = 0.2f;
+    [SerializeField] private float dashDuration = 0.2f;//대쉬 지속시간
+    [SerializeField] private float dashCoolTime = 2.0f;//대쉬 쿨타임
+    [SerializeField] private float dashSpeed = 10.0f;//대쉬 속도
 
-    [SerializeField] private float dashSpeed = 10.0f;
+    //그외
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private float dashCoolTime = 2.0f;
+    
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-
+        
+        if( Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveInput = -1f;
+        }
+        else if ( Input.GetKey(KeyCode.RightArrow))
+        {
+            moveInput = 1f;
+        }
+        else
+        {
+            moveInput = 0f;
+        }
         if (Input.GetKeyDown(KeyCode.UpArrow) && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
@@ -34,11 +53,10 @@ public class PlayerMove : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
-        if (Input.GetKeyDown(KeyCode.LeftShift) && !canDash) 
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash&&!isDash) 
         {
             StartCoroutine(dash());
         }
-
         Flip();
     }
 
@@ -59,10 +77,9 @@ public class PlayerMove : MonoBehaviour
         yield return new WaitForSeconds(dashCoolTime);
         canDash = true;
     }
-
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
     }
 
     private bool IsGrounded()
@@ -72,7 +89,7 @@ public class PlayerMove : MonoBehaviour
 
     private void Flip()
     {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        if (isFacingRight && moveInput < 0f || !isFacingRight && moveInput > 0f)
         {
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
@@ -80,6 +97,14 @@ public class PlayerMove : MonoBehaviour
             transform.localScale = localScale;
 
         }
+    }
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        /*if(coll.collider.CompareTag("Rope"))
+        {
+            Rope rope=coll.gameObject.GetComponent<Rope>();
+            rope.AnchorPlayer(this.gameObject.transform);
+        }*///로프 액션아직 구현 못함
     }
 }
 
